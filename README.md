cb7e2ba850de123c3a373b9364c0ad8bce730e1f
AshfaqKabir <ashfaqkabir@gmail.com>
2022-12-06 13:06:09 +0000

    Initial Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Veggie Mart
+
+### Shop For Your Favorite Veggies [Here](https://veggiemart.netlify.app)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Box>Everything SETUP</Box>
+      </div>
+    </div>
+  );
+}
+
+export default App;
